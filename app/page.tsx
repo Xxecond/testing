115a2b7374e279965797fbfe5ca886d8cfbc9e0b
Xxecond <136137218+Xxecond@users.noreply.github.com>
2025-10-25 13:53:11 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 export default function Home() {
   return (
-    <div className="h-120 w-full bg-red-900 text-4xl text-yellow-900">
+    <div className="h-120 grid p-10 w-full bg-red-900 text-4xl text-yellow-900">
     hey
-    <div>group</div>
+    <div bg-pink-900>group</div>
+div className="bg-blue-900">group</div>
+        
+div className="bg-cyan-900">group</div>
+div className="bg-brown-900">group</div>
     </div>
   );
 }
